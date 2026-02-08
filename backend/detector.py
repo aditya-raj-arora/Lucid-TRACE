0f3f41769f6099b8ff295fbeb02ee40bdc122d0a
Aditya Raj Arora <adityarajarora123@gmail.com>
2026-02-09 01:59:44 +0530

    detector

--- backend/detector.py
+++ backend/detector.py
@@ -1,5 +1,14 @@
+import os
 import time
 import random
+from dotenv import load_dotenv
+from lucid_trace import check_frame_with_sightengine
+
+
+load_dotenv()
+API_USER = os.getenv('API_USER')
+API_SECRET = os.getenv('API_SECRET')
+API_URL = 'https://api.sightengine.com/1.0/check.json'
 
 def analyze_frames(frame_paths):
     scores = []
@@ -7,7 +16,7 @@ def analyze_frames(frame_paths):
         time.sleep(0.4) # Simulate API latency
         
         # TODO: Replace this mock with your real Sightengine API call
-        ai_probability = random.uniform(0.3, 0.95) 
+        ai_probability = check_frame_with_sightengine(frame) 
         
         scores.append(ai_probability)
     return scores
